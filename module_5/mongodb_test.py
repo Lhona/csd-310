4b99edc93a9302194448a95822c358aab366afb5
Lhona <chrbryant2@my365.bellevue.edu>
2021-11-21 10:50:43 -0600

    Don't know what's wrong, pushing it anyway

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,16 +1,7 @@
-#from pymongo import MongoClient
-#url = "mongodb+srv://admin:admin@cluster0.wcq8x.mongodb.net/pytech?retryWrites=true&w=majority";
-#client = MongoClient(url)
-#db = client.pytech
-#print(db.list_collection_names)
-
-import pymongo
-
-myclient = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.wcq8x.mongodb.net/pytech?retryWrites=true&w=majority")
-
-#use database "organisation"
-mydb = myclient['pytech']
-
-print("List of collections\n--------------------")
-#list the collections
-print(mydb.list_collection_names)
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.wcq8x.mongodb.net/pytech?retryWrites=true&w=majority";
+client = MongoClient(url)
+db = client.pytech
+print("-- Pytech Collection List --")
+print(db.list_collection_names)
+input("End of program, press any key to exit...")
