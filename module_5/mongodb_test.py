7dc985b67b49447404d593e4a9c0d229c07b2a12
Lhona <chrbryant2@my365.bellevue.edu>
2021-11-23 13:51:03 -0600

    Removed the text, I think it was part of the program

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,7 +1,5 @@
 from pymongo import MongoClient
 url = "mongodb+srv://admin:admin@cluster0.wcq8x.mongodb.net/pytech?retryWrites=true&w=majority";
 client = MongoClient(url)
-db = client.pytech
-print("-- Pytech Collection List --")
+db = client.pytech)
 print(db.list_collection_names)
-input("End of program, press any key to exit...")
